f9af555ff72d7f4036676dee94cedb489af19a57
sason <songdixp@126.com>
2025-02-05 13:40:58 +0800

    20250204-1

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -3,9 +3,16 @@ import "hardhat/console.sol";
 
 contract Counter{
     uint num; 
+    // 合约上的事件，不仅仅时时根据block更新来监听状态变化，还能把事件保存在历史的区块上面
+    // 这就是provider 内置的方式，监听到链上的状态变化，这就是事件log，每次发生变化的时候通过filter监听的合约变化
+    // 再响应到具体的应用上来
+    event CounterInc(uint num);
+    // 这种好处是，可以传入address sender 参数，然后将调用者的信息返回出去，知道谁在调用这个方法，能解决很多场景的问题
+
     function count()public {
         num++;
         console.log("num is :", num);
+        emit CounterInc(num);
     }
 
     function getNum()public view returns(uint){
--- src/index-num.ts
+++ src/index-num.ts
@@ -3,6 +3,7 @@
 // 这里有人喜欢使用 hardhat，因为也提供ethers工具，但是这里会报错，因为index.ts的执行环境是浏览器
 // hardhat又是在nodejs环境下的工具，浏览器里面引用hardhat就会执行不了
 import {ethers} from "ethers";
+import {abi} from "../artifacts/contracts/Counter.sol/Counter.json";
 
 // src 是前端的内容，scripts 和 contracts 是后端合约的内容，要做好区分
 
@@ -54,10 +55,12 @@ async function getContract() {
     const contract = new ethers.Contract(
         address,
         // 接口，就是合约 HelloWorld 的接口信息
-        [
-            "function count()public",
-            "function getNum()public view returns(uint)" 
-        ],
+        // [
+        //     "function count()public",
+        //     "function getNum()public view returns(uint)" 
+        // ],
+        // 通过abi 的形式来导入接口信息
+        abi,
         // provider 指的就是小狐狸钱包，合约需要连接网络，单纯从eth 对象是不知道的，需要一个中介来知道连接哪个网络，谁能知道，只有小狐狸钱包
         // 需要signer的时候使用很简单
         await provider.getSigner(),
@@ -71,18 +74,45 @@ async function getContract() {
     getNum();
 
     // 设置变量的按钮
-    async function setCount() {
-        return await contract.count();
-    }
+    // async function setCount() {
+    //     return await contract.count();
+    // }
+    
     // btn按钮
     const btn = document.createElement("button");
     btn.innerHTML = "increament";
     btn.onclick = async function(){
         // 这里点击按钮之后，需要刷新页面才能拿到增加的数字，为什么？
-        const tx = await setCount(); // await 过程中，等待transcation事务提交，而不是等待transcation完成
-        await tx.wait(); //  这样就能等待tx 完成
-        getNum();
+        // const tx = await setCount(); // await 过程中，等待transcation事务提交，而不是等待transcation完成
+        // await tx.wait(); //  这样就能等待tx 完成
+        // getNum();
+        /*
+        这种开发方式是不合理的。
+        每次都是调用方法，还要等待 tx 完成，之后再修改方法，非常难受。
+        */ 
+    /*
+    我们希望能够监听到合约的变化，然后还通知你来进行改变，才是合理的。
+    因此需要再合约里面写一个事件 CounterInc
+    1.重新部署Counter合约
+    2.将新合约的地址放到 .env 环境变量中
+    2.1 在修改Contract 接口的列表，但实际生产中不需要这么麻烦，通过abi的形式来获取接口
+    3.重启webpack
+    这三个步骤是重复的，习惯之后就熟悉了，也可以通过脚本完成这一套操作
+    */    
+        // 修改方法,点击之后不在去获取状态，而是增加事件
+        await contract.count();
     };
+
+    // 增加监听事件, 在ethers里面通过filter来拿到事件
+    // 在ethers 6之后，不再是传 参数列表过来，而是传 payload {args 对象包含参数列表其他信息}
+    // 这些信息可以看官方文档 ??? 这里看的是哪个官方文档？
+    contract.on(contract.filters.CounterInc(), async function({args}){
+        // 事件监听常用的是这种方式，并且后台应用也会使用这种方式
+        // 事件发出的时候，后台监听到事件，将事件状态内容存储到后台数据库里面
+        // 在区块链上的接口访问是有一些限制的，大量访问的话，有峰值，不会让你在节点上访问过多次数，工程话的解决方案是：将链上的状态存储到链下缓解链上的网络压力
+        count.innerHTML = args[0].toString() || await contract.getCount();
+    });
+
 
     // 将创建好的两个元素添加到document body 中
     document.body.appendChild(count);
